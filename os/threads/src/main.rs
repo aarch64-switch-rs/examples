05c4779732e85b887be977ed762832c3a2da4efb
XorTroll <xabofdez@gmail.com>
2021-08-27 04:02:43 +0200

    Update for latest lib changes

--- os/threads/src/main.rs
+++ os/threads/src/main.rs
@@ -31,25 +31,26 @@ pub fn initialize_heap(hbl_heap: util::PointerAndSize) -> util::PointerAndSize {
 pub fn main() -> Result<()> {
     diag_log!(log::LmLogger { log::LogSeverity::Trace, false } => "Starting threads...\n");
 
-    let mut t1 = thread::Thread::new(move || {
+    let mut t1 = thread::Thread::new(move |&(i1, i2, i3, c1, c2, c3, c4)| {
+        diag_log!(log::LmLogger { log::LogSeverity::Trace, false } => "Thread 1 sample params: {} {} {} {} {} {} {}\n", i1, i2, i3, c1, c2, c3, c4);
         for i in 0..5 {
             diag_log!(log::LmLogger { log::LogSeverity::Trace, false } => "Hi from thread 1 (ID: {}) {}\n", thread::get_current_thread().get_id().unwrap(), i);
         }
-    }, "Thread1", 0x1000)?;
+    }, &(1, 2, 3, 'c', 'd', 'b', 'a'), "Thread1", 0x1000)?;
     t1.initialize(thread::PRIORITY_AUTO, -2)?;
 
-    let mut t2 = thread::Thread::new(move || {
+    let mut t2 = thread::Thread::new(move |()| {
         for i in 0..5 {
             diag_log!(log::LmLogger { log::LogSeverity::Trace, false } => "Ohayo from thread 2 (ID: {}) {}\n", thread::get_current_thread().get_id().unwrap(), i);
         }
-    }, "Thread2", 0x1000)?;
+    }, &(), "Thread2", 0x1000)?;
     t2.initialize(thread::PRIORITY_AUTO, -2)?;
 
-    let mut t3 = thread::Thread::new(move || {
+    let mut t3 = thread::Thread::new(move |()| {
         for i in 0..5 {
             diag_log!(log::LmLogger { log::LogSeverity::Trace, false } => "Sup from thread 3 (ID: {}) {}\n", thread::get_current_thread().get_id().unwrap(), i);
         }
-    }, "Thread3", 0x1000)?;
+    }, &(), "Thread3", 0x1000)?;
     t3.initialize(thread::PRIORITY_AUTO, -2)?;
 
     diag_log!(log::LmLogger { log::LogSeverity::Trace, false } => "Thread IDs: {} {} {}\n", t1.get_id()?, t2.get_id()?, t3.get_id()?);
